0ba4c3559fc2e15a0c9e86677c9a94cb391ad5c4
Michał Kowalczewski <michal.kowalczewski@atos.net>
2020-05-19 16:04:47 +0200

    fix

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/sda")
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
