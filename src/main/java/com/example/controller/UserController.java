aa56dc9e405a71ddccd218e76b8ba0ff6f31c77c
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-02-19 20:28:35 +0100

    comment

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -8,21 +8,33 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/user")
+//@SessionScope -  bean tworzony raz na sesje użytkownika (jeżeli otworzymy aplikacje np w innej przeglądarce, to zobaczymy w konsoli inicjalizacje)
+//@RequestScoped - przy każdym odwołaniu się do beana jest tworzony nowy bean
+//@ApplicationScoped - bean tworzony raz na całe życie aplikacji - używany może być np do połączeń z bazą danych
 public class UserController {
 
     @Autowired
     UserDAO userDAO;
 
+    @PostConstruct
+    public void init(){
+        System.out.println("zainicjalizowano stronę");
+    }
+
+    //pobieramy id ze ścieżki, przesyłamy do argumentu metody za pomocą adnotacji @PathVariable
     @RequestMapping("/main/{id}")
     public String mainPageId(@PathVariable("id") int id,
                            Model model){
+        //dostarczamy atrybut "users" który będzie dostępny w warstwie widoku
         model.addAttribute("user", userDAO.getUserById(id));
         return "index";
     }
+
 
     @RequestMapping("/main_param")
     public String mainPageParamId(@RequestParam("id") int id, Model model){
@@ -30,6 +42,7 @@ public class UserController {
         return "index";
     }
 
+    //index.html
     @RequestMapping("/main")
     public String mainPage(Model model){
         model.addAttribute("users", userDAO.getUsers());
